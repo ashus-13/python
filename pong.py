0259bd64c60f11772e5e58c7a8393f1a17f0b40e
ashus-13 <ashutosh.soccer@gmail.com>
2024-01-13 21:32:39 +0530

    pong game completed

--- pong.py
+++ pong.py
@@ -6,6 +6,10 @@ wn.bgcolor('black')
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
+
 
 #Paddle A
 paddle_a = turtle.Turtle()
@@ -35,6 +39,15 @@ ball.penup()
 ball.goto(0,0)
 ball.dx = 0.06
 ball.dy = 0.06
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align='center', font=('Courier', 12, 'normal'))
 
 #Functions
 def paddle_a_up():
@@ -83,3 +96,32 @@ while True:
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx = 0.06
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align='center', font=('Courier', 12, 'normal'))
+
+    
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx = 0.06
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align='center', font=('Courier', 12, 'normal'))
+
+
+    #Paddle and ball collision
+    if(ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40
+        and ball.ycor() > paddle_b.ycor() - 40) ):
+        ball.setx(340)
+        ball.dx *= -1.03
+    if(ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() + 40
+        and ball.ycor() > paddle_a.ycor() - 40) ):
+        ball.setx(-340)
+        ball.dx *= -1.03
+    
